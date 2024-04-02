77e7548d9872290db3a26ede8445e96d66b07ceb
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-04-02 22:35:07 +0800

    finished project

--- beginner/share-content-card-component/script.js
+++ beginner/share-content-card-component/script.js
@@ -2,31 +2,29 @@
 
 const inputGroup = document.querySelector(".input-group");
 const input = document.querySelector(".input");
+const share = document.querySelector(".share");
 
-// const Toast = Swal.mixin({
-//   toast: true,
-//   icon: "success",
-//   title: "Text copied successfully",
-//   showConfirmButton: false,
-//   timer: 2000,
-//   didOpen: (toast) => {
-//     toast.onmouseenter = Swal.stopTimer;
-//     toast.onmouseleave = Swal.resumeTimer;
-//   },
-// });
+const Toast = Swal.mixin({
+  toast: true,
+  icon: "success",
+  color: "#ede9e2",
+  background: "#1e1411",
+  width: "12em",
+  title: "Text copied successfully",
+  showConfirmButton: false,
+  position: "top-end",
+  timer: 2000,
+  target: share,
+  customClass: {
+    container: "position-absolute",
+  },
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  },
+});
 
 inputGroup.addEventListener("click", () => {
   navigator.clipboard.writeText(input.value);
-
-  // Toast.fire({
-  //   target: document.querySelector(".share"),
-  //   position: "top-end",
-  //   customClass: {
-  //     container: "share",
-  //   },
-  // });
-});
-
-input.addEventListener("click", () => {
-  navigator.clipboard.writeText(input.value);
+  Toast.fire();
 });
