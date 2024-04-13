e60d7075cd02f77de35ae45f7c543e8ff36b47d1
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-04-13 15:23:05 +0800

    finished project

--- intermediate/accordion-component/script.js
+++ intermediate/accordion-component/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const faq = document.querySelector(".faq__list");
+const questions = document.querySelectorAll(".faq__item");
+
+const showAnswer = (e) => {
+  const clicked = e.target.closest(".faq__item");
+  questions.forEach((question) => question.classList.remove("active"));
+  clicked.classList.toggle("active");
+};
+
+faq.addEventListener("click", showAnswer);
