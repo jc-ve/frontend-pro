9a6bba5eed3847b7c7e1fb8a34a7a1ea5f1b35ed
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-04-26 16:31:14 +0800

    added tab functionality

--- projects/intermediate/icon-sets-pricing-card/script.js
+++ projects/intermediate/icon-sets-pricing-card/script.js
@@ -2,11 +2,22 @@
 
 const listPricingPlan = document.querySelector(".pricing__plan");
 const itemsPricingPlan = document.querySelectorAll(".pricing__item");
+const contentsPricingPlan = document.querySelectorAll(".pricing__body");
 
 const showPlan = (e) => {
   const clicked = e.target.closest(".pricing__item");
+
+  if (!clicked) return;
+
+  // Remove active classes
   itemsPricingPlan.forEach((item) => item.classList.remove("active"));
+  contentsPricingPlan.forEach((content) => content.classList.remove("active"));
+
   clicked.classList.add("active");
+
+  document
+    .querySelector(`.pricing__body--${clicked.dataset.plan}`)
+    .classList.add("active");
 };
 
 listPricingPlan.addEventListener("click", showPlan);
