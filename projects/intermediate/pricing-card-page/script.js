13e61e2f03ca087e7ea2affc7e4241d08b795621
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-04-29 20:26:48 +0800

    added button animations

--- projects/intermediate/pricing-card-page/script.js
+++ projects/intermediate/pricing-card-page/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const btnControl = document.querySelector(".btn--control");
+const btnIndicator = document.querySelector(".btn__indicator");
+
+const slideIndicator = () => {
+  btnIndicator.classList.toggle("active");
+};
+
+btnControl.addEventListener("click", slideIndicator);
